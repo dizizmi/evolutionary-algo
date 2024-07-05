d5811f9083136d04cf25cc201df24749567dcee2
dizizmi <65617120+dizizmi@users.noreply.github.com>
2024-07-05 22:48:02 +0800

    added more parameters to test but. doesnt finish testing all

--- test_ga_no_threads.py
+++ test_ga_no_threads.py
@@ -13,52 +13,67 @@ import numpy as np
 import csv
 
 class TestGA(unittest.TestCase):
-    def testBasicGA(self):
-        pop = population.Population(pop_size=10, 
-                                    gene_count=3)
+    def testBasicGA(self, pop_size, gene_count, point_mutate_rate, shrink_mutate_rate, grow_mutate_rate):
+        pop = population.Population(pop_size=pop_size, 
+                                    gene_count=gene_count)
         #sim = simulation.ThreadedSim(pool_size=1)
         sim = simulation.Simulation()
 
         results = []
         elites = []
 
-        for iteration in range(100):
+        for iteration in range(40):
             # this is a non-threaded version 
             # where we just call run_creature instead
             # of eval_population
             for cr in pop.creatures:
                 sim.run_creature(cr, 2400)            
             #sim.eval_population(pop, 2400)
+
             fits = [cr.get_distance_travelled() 
                     for cr in pop.creatures]
             links = [len(cr.get_expanded_links()) 
                     for cr in pop.creatures]
+            
             print(iteration, "fittest:", np.round(np.max(fits), 3), 
                   "mean:", np.round(np.mean(fits), 3), "mean links", np.round(np.mean(links)), "max links", np.round(np.max(links)))       
+            results.append([iteration, np.round(np.max(fits), 3), np.round(np.mean(fits), 3), np.round(np.mean(links)), np.round(np.max(links))])
+
+            
+
+            #GA operators
             fit_map = population.Population.get_fitness_map(fits)
             new_creatures = []
+
             for i in range(len(pop.creatures)):
                 p1_ind = population.Population.select_parent(fit_map)
                 p2_ind = population.Population.select_parent(fit_map)
                 p1 = pop.creatures[p1_ind]
                 p2 = pop.creatures[p2_ind]
-                # now we have the parents!
+
+                # crossover and mutations
                 dna = genome.Genome.crossover(p1.dna, p2.dna)
                 dna = genome.Genome.point_mutate(dna, rate=0.1, amount=0.25)
                 dna = genome.Genome.shrink_mutate(dna, rate=0.25)
                 dna = genome.Genome.grow_mutate(dna, rate=0.1)
+
+                #create new creature
                 cr = creature.Creature(1)
                 cr.update_dna(dna)
                 new_creatures.append(cr)
+
             # elitism
             max_fit = np.max(fits)
+            
             for cr in pop.creatures:
                 if cr.get_distance_travelled() == max_fit:
                     new_cr = creature.Creature(1)
                     new_cr.update_dna(cr.dna)
+
                     new_creatures[0] = new_cr
                     filename = "elite_"+str(iteration)+".csv"
                     genome.Genome.to_csv(cr.dna, filename)
+
                     break
             
             pop.creatures = new_creatures
@@ -73,29 +88,33 @@ class TestGA(unittest.TestCase):
             writer.writerow(["Iteration", "Max Fitness", "Mean Fitness", "Mean Links", "Max Links"])
             writer.writerows(results)
 
-        # Save all elite genomes to a single CSV file
-        with open('all_elites.csv', mode='w', newline='') as elite_file:
-            elite_writer = csv.writer(elite_file)
-            for elite in elites:
-                iteration, dna = elite
-                elite_writer.writerow([iteration] + dna)
+
 
         return results
 
     
-    def testBasicGA2(self):
+    def test_multiple(self):
         experiments = [
             {"pop_size": 10, "gene_count": 3, "point_mutate_rate": 0.1, "shrink_mutate_rate": 0.25, "grow_mutate_rate": 0.1},
             {"pop_size": 20, "gene_count": 4, "point_mutate_rate": 0.2, "shrink_mutate_rate": 0.2, "grow_mutate_rate": 0.2},
+            {"pop_size": 50, "gene_count": 3, "point_mutate_rate": 0.2, "shrink_mutate_rate": 0.2, "grow_mutate_rate": 0.2},
+            {"pop_size": 50, "gene_count": 5, "point_mutate_rate": 0.3, "shrink_mutate_rate": 0.2, "grow_mutate_rate": 0.2},
+            # Variations in mutation rates
+            {"pop_size": 20, "gene_count": 4, "point_mutate_rate": 0.3, "shrink_mutate_rate": 0.1, "grow_mutate_rate": 0.3},
+            {"pop_size": 20, "gene_count": 4, "point_mutate_rate": 0.4, "shrink_mutate_rate": 0.2, "grow_mutate_rate": 0.1},
+            # Larger population sizes
+            {"pop_size": 100, "gene_count": 5, "point_mutate_rate": 0.2, "shrink_mutate_rate": 0.2, "grow_mutate_rate": 0.2},
+            {"pop_size": 200, "gene_count": 6, "point_mutate_rate": 0.3, "shrink_mutate_rate": 0.2, "grow_mutate_rate": 0.2},
+            # More complex gene structures
+            {"pop_size": 50, "gene_count": 10, "point_mutate_rate": 0.2, "shrink_mutate_rate": 0.2, "grow_mutate_rate": 0.2},
         ]
-        
 
-        with open('experiment_results.csv', mode='w', newline='') as file:
+        with open('experiments_result.csv', mode='w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(["Experiment", "Iteration", "Max Fitness", "Mean Fitness", "Mean Links", "Max Links"])
-            
+            writer.writerow(["Pop Size", "Gene Count", "Point Mutate Rate", "Shrink Mutate Rate", "Grow Mutate Rate", "Iteration", "Max Fitness", "Mean Fitness", "Mean Links", "Max Links"])
+
             for idx, params in enumerate(experiments):
-                results = self.run_experiment(
+                results = self.testBasicGA(
                     pop_size=params["pop_size"],
                     gene_count=params["gene_count"],
                     point_mutate_rate=params["point_mutate_rate"],
@@ -103,6 +122,8 @@ class TestGA(unittest.TestCase):
                     grow_mutate_rate=params["grow_mutate_rate"]
                 )
                 for result in results:
-                    writer.writerow([idx] + result)
+                    writer.writerow([params["pop_size"], params["gene_count"], params["point_mutate_rate"], params["shrink_mutate_rate"], params["grow_mutate_rate"]] + result)
+
+            #cant get creature to climb mountain so cant test duration completed..... 
 
 unittest.main()
--- test_simulation.py
+++ test_simulation.py
@@ -42,7 +42,8 @@ class TestSim(unittest.TestCase):
 #        self.assertIsNotNone(dists)
 
     def testProcNoThread(self):
-        pop = population.Population(pop_size=20, gene_count=3)
+        #based on graph that reaches max fitness
+        pop = population.Population(pop_size=20, gene_count=4)
         sim = simulation.Simulation()
         sim.eval_population(pop, 2400)
         dists = [cr.get_distance_travelled() for cr in pop.creatures]
